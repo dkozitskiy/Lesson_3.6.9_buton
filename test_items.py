592a67a6264e10749ea0b0fe3d03061e0e3c168a
Denis <dkozitskiy@gmail.com>
2021-12-12 23:28:09 +0200

    Add to basket button is missing on this page

--- test_items.py
+++ test_items.py
@@ -7,7 +7,7 @@ def test_guest_should_see_add_to_basket_button(browser):
         browser.get(link)
         time.sleep(10)
         btn = browser.find_element_by_css_selector(".btn-add-to-basket")
-        assert btn
+        assert btn, "Add to basket button is missing on this page"
         print("!!" * 20)
         print(f"Текст на кнопке: '{btn.text}'")
         print("!!" * 20)
